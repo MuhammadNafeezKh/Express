0929f3fed5e6d847ca65d093441b1d0cfd002acf
Nafeez <nafismuhammad277@gmail.com>
2026-04-24 10:23:51 +0700

    Update app.js

--- web-db-sederhana/app.js
+++ web-db-sederhana/app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const bodyParser = require("body-parser");
-const path = require("path");  // ✅ Ditambahkan
+const path = require("path"); 
 
 const app = express();
 const PORT = 3000;
 
 // middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));  // ✅ Diubah
+app.use(express.static(path.join(__dirname, "public"))); 
 
 // database
 const db = new sqlite3.Database("database.db");
